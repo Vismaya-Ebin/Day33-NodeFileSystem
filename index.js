156566970a6538e45704f074a8e313298657c607
Vismaya Wilson <vismayawilson3@gmail.com>
2022-03-18 11:12:12 +0530

    initial

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs"); //type common.js
 
+
 const date_Time = new Date().toLocaleString();
 
 //To create a file date-time.txt with timestamp details
@@ -14,10 +15,10 @@ fs.writeFile("current date-time.txt", date_Time, (err) => {
 
 //To retrieve the data from a folder and
 // create a folder with files
-const content = "💕💕💕💕👩‍❤️‍👨👩‍❤️‍👨👩‍❤️‍👨👩‍❤️‍👨👩‍❤️‍👨❤️❤️❤️❤️"
+const content = `💕💕💕💕👩‍❤️‍👨👩‍❤️‍👨👩${date_Time}👩‍❤️‍👨👩‍❤️‍👨❤️❤️❤️❤️`;
 for(let i = 1;i <= 10;i++){
     fs.writeFile(`./folder/text-${i}`,content,(err)=>{
-        console.log(`${i} file created`);
+        console.log(`${i} file created ${date_Time}`);
     });
 
     fs.readFile(`./folder/text-${i}`, "utf-8",(err,data)=>
